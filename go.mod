0ecfa8e4c0fb183709c7097ee439ac31b951ff2b
sachin-ghait-cld <sachin.ghait@cldcvr.com>
2021-03-20 23:02:46 +0530

    added logger to utils

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sachin-ghait-cld/bookstore_utils-go
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/zap v1.16.0
+)
---
+++ logger/logger.go
@@ -0,0 +1,93 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	envLogLevel  = "LOG_LEVEL"
+	envLogOutput = "LOG_OUTPUT"
+)
+
+var (
+	log logger
+)
+
+type logger struct {
+	log *zap.Logger
+}
+type loggerIfc interface {
+	Printf(string, ...interface{})
+}
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
+
+func getLevel() zapcore.Level {
+	switch os.Getenv(envLogLevel) {
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	case "debug":
+		return zap.DebugLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func getOutput() string {
+	output := strings.TrimSpace(os.Getenv(envLogOutput))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func GetLogger() loggerIfc {
+	return log
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+
+}
+
+// Info Info
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
+
+// Error Error
+func Error(msg string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.log.Error(msg, tags...)
+	log.log.Sync()
+}
